223142e90ba082470f6c6da106c328634c639d8f
Abdus-Salam-814 <abdussalam6111997@gmail.com>
2025-12-28 09:00:58 +0600

    i learn JavaScript

---
+++ intruduction.js
@@ -0,0 +1,9 @@
+// console.log(33)
+
+
+let i = 101;
+
+while(i > 1){
+    i-=2
+    // console.log(i)
+}
---
+++ variable.js
@@ -0,0 +1,8 @@
+var firstName = 'Abdus';
+var lastName = 'Salam';
+
+var fullName = firstName + " " + lastName;
+
+console.log(fullName)
+
+console.log(fullName, "type is:", typeof fullName)
